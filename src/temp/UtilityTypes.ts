49648de81ece19547387b96e4624bc0261e86299
Abhishek Kashyap <abhishek.k@amberstudent.com>
2023-06-19 10:21:57 +0530

    feat(navbar): add navlist item type

--- src/temp/UtilityTypes.ts
+++ src/temp/UtilityTypes.ts
@@ -1,13 +1,10 @@
+//www.typescriptlang.org/docs/handbook/utility-types.html
+
 export type User = {
   name: string;
   age: number;
-  email: `${string}@${string}.com`;
+  email: `${string}@${string}.com`; // String literal type
   phone?: number;
-};
-
-const user: User = {
-  name: "Abc",
-  age: 23,
 };
 
 // Required
@@ -19,4 +16,14 @@ export type UserWithPartialValues = Partial<User>;
 // Readonly
 export type UserWithReadonlylValues = Readonly<User>;
 
-// 
+// Omit
+export type UserWithoutEmail = Omit<User, "email">;
+
+// Pick
+export type UserWithNameAndPhone = Pick<User, "name" | "phone">;
+
+// String Manipulation types
+// Uppercase<StringType>
+// Lowercase<StringType>
+// Capitalize<StringType>
+// Uncapitalize<StringType>
