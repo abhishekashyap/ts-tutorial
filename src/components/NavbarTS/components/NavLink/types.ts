21387893e4ea52d34a5e58b648ec7537ccee019b
Abhishek Kashyap <abhishek.k@amberstudent.com>
2023-06-19 10:05:27 +0530

    feat(navbar): add navlist item type

--- src/components/NavbarTS/components/NavLink/types.ts
+++ src/components/NavbarTS/components/NavLink/types.ts
@@ -1,14 +1,15 @@
 import { TablerIconsProps } from "@tabler/icons-react";
 import { MouseEvent } from "react";
+import { NavItemLabel } from "../../constants/navItemList";
 
 type NavItem = {
   link: string;
-  label: string;
+  label: NavItemLabel;
   icon: (props: TablerIconsProps) => JSX.Element;
 };
 
 export type NavLinkProps = {
   item: NavItem;
   isActive: boolean;
-  onClick: (event: MouseEvent<HTMLAnchorElement>, label: string) => void;
+  onClick: (event: MouseEvent<HTMLAnchorElement>, label: NavItemLabel) => void;
 };
